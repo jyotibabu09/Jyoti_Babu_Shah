200016310ba49bd693dde8f0525e8a94e832e10d
jyotibabu09 <jyotibabushah09@gmail.com>
2024-12-03 16:10:21 +0545

    Email changed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(' adrian@jsmastery.pro');
+    navigator.clipboard.writeText(' jyotibabushah09@gmail.com');
     setHasCopied(true);
 
     setTimeout(() => {
